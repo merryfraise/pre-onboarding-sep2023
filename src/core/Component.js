0e3c3f5643717f3a33b45ce5b8228a3fd2aee1bc
Shoupeach <shoutrawberry@gmail.com>
2023-08-31 21:33:19 +0900

    🍒 feat: Feature of core Component

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,1 +1,19 @@
-export default class Component {}
+export default class Component {
+  $target;
+
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+  }
+
+  mounted() {}
+
+  template() {
+    return '';
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.mounted();
+  }
+}

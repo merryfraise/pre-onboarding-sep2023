6277fb3d803df4d0712bdb0fe4729f9010db5f65
Shoupeach <shoutrawberry@gmail.com>
2023-08-31 22:03:32 +0900

    🍒 feat: Button White component

---
+++ src/components/ButtonWhite.js
@@ -0,0 +1,9 @@
+import Component from '../core/Component.js';
+
+export default class ButtonWhite extends Component {
+  template() {
+    return `
+        <button>READ MORE/button>
+    `;
+  }
+}
